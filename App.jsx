bed161cedd367f854f9e9d33214debf0aca57df1
박성춘 <songchoon10@naver.com>
2024-05-28 22:17:34 +0900

    feat: 음성인식 기능 구현

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -10,26 +10,26 @@ import {
   SafeAreaView,
   Platform,
   PermissionsAndroid,
-} from 'react-native';
-import Voice from '@react-native-voice/voice';
+} from "react-native";
+import Voice from "@react-native-voice/voice";
 
 const App = () => {
   const [messages, setMessages] = useState([]);
   const [isListening, setIsListening] = useState(false);
-  const [recognizedText, setRecognizedText] = useState('');
+  const [recognizedText, setRecognizedText] = useState("");
 
   useEffect(() => {
     Voice.onSpeechStart = onSpeechStart;
     Voice.onSpeechEnd = stopListening;
     Voice.onSpeechResults = onSpeechResults;
-    Voice.onSpeechError = (error) => console.log('onSpeechError:', error);
+    Voice.onSpeechError = (error) => console.log("onSpeechError:", error);
 
     const androidPermissionChecking = async () => {
-      if (Platform.OS === 'android') {
+      if (Platform.OS === "android") {
         const hasPermission = await PermissionsAndroid.check(
-          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO
         );
-        console.log('Android permission check: ', hasPermission);
+        console.log("Android permission check: ", hasPermission);
       }
     };
     androidPermissionChecking();
@@ -49,30 +49,30 @@ const App = () => {
     setRecognizedText(text);
   };
 
-  const startListening = async() => {
+  const startListening = async () => {
     setIsListening(true);
-    try{
-      await Voice.start("en-US");
-    } catch(error) {
+    try {
+      // await Voice.start("en-US");
+      await Voice.start("ko-KR");
+    } catch (error) {
       console.log("line error ~ : ", error);
     }
-  }
+  };
 
-  const stopListening = async() => {
+  const stopListening = async () => {
     try {
       await Voice.stop();
       Voice.removeAllListeners();
       setIsListening(false);
     } catch (error) {
       console.log("line error ~ : ", error);
-      
     }
-  }
+  };
 
   const sendMessage = () => {
     if (recognizedText) {
-      setMessages([...messages, {text: recognizedText, sender: 'user'}]);
-      setRecognizedText('');
+      setMessages([...messages, { text: recognizedText, sender: "user" }]);
+      setRecognizedText("");
     }
   };
 
@@ -87,11 +87,12 @@ const App = () => {
               styles.messageBubble,
               {
                 alignSelf:
-                  message.sender === 'user' ? 'flex-end' : 'flex-start',
+                  message.sender === "user" ? "flex-end" : "flex-start",
                 backgroundColor:
-                  message.sender === 'user' ? '#BB2525' : '#141E46',
+                  message.sender === "user" ? "#BB2525" : "#141E46",
               },
-            ]}>
+            ]}
+          >
             <Text style={styles.messageText}>{message.text}</Text>
           </View>
         ))}
@@ -101,21 +102,22 @@ const App = () => {
           style={styles.input}
           placeholder="Type your message..."
           value={recognizedText}
-          onChangeText={text => setRecognizedText(text)}
+          onChangeText={(text) => setRecognizedText(text)}
         />
         <TouchableOpacity
           onPress={() => {
             isListening ? stopListening() : startListening();
           }}
-          style={styles.voiceButton}>
+          style={styles.voiceButton}
+        >
           {isListening ? (
             <Text style={styles.voiceButtonText}>•••</Text>
           ) : (
             <Image
               source={{
-                uri: 'https://cdn-icons-png.flaticon.com/512/4980/4980251.png',
+                uri: "https://cdn-icons-png.flaticon.com/512/4980/4980251.png",
               }}
-              style={{width: 45, height: 45}}
+              style={{ width: 45, height: 45 }}
             />
           )}
         </TouchableOpacity>
@@ -130,35 +132,35 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF5E0',
+    backgroundColor: "#FFF5E0",
   },
   messagesContainer: {
     padding: 10,
   },
   messageBubble: {
-    maxWidth: '70%',
+    maxWidth: "70%",
     marginVertical: 5,
     borderRadius: 10,
     padding: 10,
   },
   messageText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     padding: 10,
     borderTopWidth: 1,
-    borderColor: '#ccc',
-    backgroundColor: 'white',
+    borderColor: "#ccc",
+    backgroundColor: "white",
   },
   input: {
     flex: 1,
     fontSize: 16,
     padding: 10,
     borderRadius: 20,
-    backgroundColor: '#EFEFEF',
+    backgroundColor: "#EFEFEF",
   },
   voiceButton: {
     marginLeft: 10,
@@ -172,11 +174,11 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     paddingHorizontal: 20,
     paddingVertical: 10,
-    backgroundColor: '#FF6969',
+    backgroundColor: "#FF6969",
     borderRadius: 20,
   },
   sendButtonText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
   },
 });
